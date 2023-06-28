a9a28faab4fc4f953c64ad51f8f1f6a3ff3cb7c1
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2023-06-28 01:11:33 +0100

    reformat code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-use std::net::TcpListener;
-
-use actix_web::{App, HttpServer, web};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
@@ -9,13 +8,14 @@ pub mod configuration;
 pub mod routes;
 pub mod startup;
 
-
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-    }).listen(listener)?.run();
+    })
+    .listen(listener)?
+    .run();
     // No .await here!
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -17,7 +17,6 @@ fn spawn_app() -> String {
     format!("http://127.0.0.1:{}", port)
 }
 
-
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
@@ -38,7 +37,6 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
@@ -56,7 +54,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
-        .header("Content-Type", "application/x-www-form-urlencoded").body(body)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
         .send()
         .await
         .expect("Failed to execute request.");
@@ -65,7 +64,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",).fetch_one(&mut connection)
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
         .await
         .expect("Failed to fetch saved subscription.");
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
@@ -77,12 +77,17 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let app_address = spawn_app();
     let client = reqwest::Client::new();
-    let test_cases = vec![("name=le%20guin", "missing the email"), ("email=ursula_le_guin%40gmail.com", "missing the name"), ("", "missing both name and email")];
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
     for (invalid_body, error_message) in test_cases {
         // Act
         let response = client
             .post(&format!("{}/subscriptions", &app_address))
-            .header("Content-Type", "application/x-www-form-urlencoded").body(invalid_body)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
             .send()
             .await
             .expect("Failed to execute request.");
